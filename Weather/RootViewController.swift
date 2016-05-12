abb0c1262c7be701dfc342bbf64ac037d7ffbfb8
Matt Deuschle <mdeuschle@gmail.com>
2016-05-12 11:32:06 -0700

    set up storyboard

---
+++ Weather/RootViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RootViewController.swift
+//  Weather
+//
+//  Created by Matt Deuschle on 5/12/16.
+//  Copyright © 2016 Matt Deuschle. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
